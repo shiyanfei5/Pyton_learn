efa2c02b1c5724feebe7837d758d1cb90841b5cf
shiyanfei5 <13218016202@163.com>
2018-12-13 13:37:22 +0800

    Initial commit

---
+++ MyQueue.py
@@ -0,0 +1,39 @@
+from threading import Lock,RLock,Condition
+from collections import deque
+
+class MyQueue(object):
+
+    def __init__(self , maxsize  ):
+        self.maxsize = maxsize
+        self._queue = deque()   #deque本身就是线程安全的
+        self.get_lock = Lock()  # 该锁用于保证 读和写的互斥，即保证读时不发生写入
+
+
+    def is_empty(self,):
+        with self.get_lock:
+            #占用该锁，防止有其他线程写入
+            if len(self._queue) == 0:
+                return True
+            else:
+                return False
+
+    def is_full(self):
+        #判断_queue的大小是否为maxsize
+        with self.get_lock:
+            #占用该锁，防止有其他线程写入
+            if len(self._queue) == self.maxsize:
+                return True
+            else:
+                return False
+
+    def get_size(self):
+        with self.get_lock:
+            #占用该锁，防止有其他线程写入
+            return len(self._queue)
+
+    def join(self):
+        pass
+
+    def task_done(self):
+        pass
+
---
+++ thread_lock_rlock.py
@@ -0,0 +1,20 @@
+import threading
+import time
+from threading import Lock,RLock,Condition,Semaphore
+from queue import Queue
+import threading
+import time
+
+semaphore = threading.Semaphore(2)
+
+def func():
+    while True:
+        if semaphore.acquire():
+            print(threading.currentThread().getName() + ' get semaphore')
+        semaphore.release()
+        time.sleep(3)
+
+
+for i in range(4):
+    t1 = threading.Thread(target=func)
+    t1.start()
